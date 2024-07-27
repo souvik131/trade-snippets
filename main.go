eec5fce8e9202195c49224a1619ab6a27aafc8c7
Souvik Das <souvik131@gmail.com>
2024-07-27 17:13:40 +0530

    [feat] enabling multiple websockets

--- main.go
+++ main.go
@@ -24,36 +24,36 @@ func main() {
 	color.Green(fmt.Sprintf("Account login took %v seconds", time.Since(start).Seconds()))
 
 	k.TickerClients = []*kite.TickerClient{}
-	i := 0
-	// for i := 0; i < 2; i++ {
-	ticker, err := k.GetWebSocketClient(&ctx)
-	if err != nil {
-		color.Red(fmt.Sprintf("%v", err))
-		return
+
+	for i := 0; i < 2; i++ {
+		ticker, err := k.GetWebSocketClient(&ctx)
+		if err != nil {
+			color.Red(fmt.Sprintf("%v", err))
+			return
+		}
+		ticker.Id = i
+		k.TickerClients = append(k.TickerClients, ticker)
+		k.TickSymbolMap = map[string]kite.KiteTicker{}
+		go func(t *kite.TickerClient) {
+			for range t.ConnectChan {
+				color.HiBlue(fmt.Sprintf("Websocket is connected in %v seconds %v", time.Since(start).Seconds(), i))
+				color.HiCyan("Subscribing Ticks")
+				start = time.Now()
+				t.SubscribeLTP(&ctx, []string{"NIFTY 50", "TATAMOTORS"})
+				t.SubscribeQuote(&ctx, []string{"INFY"})
+				t.SubscribeFull(&ctx, []string{"ACC"})
+			}
+		}(k.TickerClients[i])
+		go func(t *kite.TickerClient) {
+			for tick := range t.TickerChan {
+				color.HiWhite(fmt.Sprintf("\nTick %v: %+v %v\n", tick.TradingSymbol, tick, i))
+				t.Unsubscribe(&ctx, []string{"NIFTY 50", "TATAMOTORS", "INFY"})
+				color.HiCyan(fmt.Sprintf("Received in %v secs", time.Since(start).Seconds()))
+			}
+		}(k.TickerClients[i])
+		go k.TickerClients[i].Serve(&ctx)
+		<-time.After(time.Second)
 	}
-	ticker.Id = i
-	k.TickerClients = append(k.TickerClients, ticker)
-	k.TickSymbolMap = map[string]kite.KiteTicker{}
-	go func(t *kite.TickerClient) {
-		for range t.ConnectChan {
-			color.HiBlue(fmt.Sprintf("Websocket is connected in %v seconds %v", time.Since(start).Seconds(), i))
-			color.HiCyan("Subscribing Ticks")
-			start = time.Now()
-			t.SubscribeLTP(&ctx, []string{"NIFTY 50", "TATAMOTORS"})
-			t.SubscribeQuote(&ctx, []string{"INFY"})
-			t.SubscribeFull(&ctx, []string{"ACC"})
-		}
-	}(k.TickerClients[i])
-	go func(t *kite.TickerClient) {
-		for tick := range t.TickerChan {
-			color.HiWhite(fmt.Sprintf("\nTick %v: %+v %v\n", tick.TradingSymbol, tick, i))
-			t.Unsubscribe(&ctx, []string{"NIFTY 50", "TATAMOTORS", "INFY"})
-			color.HiCyan(fmt.Sprintf("Received in %v secs", time.Since(start).Seconds()))
-		}
-	}(k.TickerClients[i])
-	go k.TickerClients[i].Serve(&ctx)
-	<-time.After(time.Second)
-	// }
 	wg := &sync.WaitGroup{}
 	wg.Add(1)
 	wg.Wait()
