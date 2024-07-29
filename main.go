f7f182109dd8ab5bca1de9a951e1eceaf0494f34
Souvik Das <souvik131@gmail.com>
2024-07-29 17:33:47 +0530

    [refactor] supports do app

--- main.go
+++ main.go
@@ -3,7 +3,9 @@ package main
 import (
 	"context"
 	"log"
-	"sync"
+	"net/http"
+	"os"
+	"path/filepath"
 
 	"github.com/souvik131/trade-snippets/kite"
 )
@@ -19,8 +21,41 @@ func main() {
 	}
 
 	Serve(&ctx, k)
-	wg := &sync.WaitGroup{}
-	wg.Add(1)
-	wg.Wait()
+	Host()
 
+}
+func Host() {
+	// Define the directory to serve
+	dir := "./binary"
+
+	// Create a file server handler
+	fileServer := http.FileServer(http.Dir(dir))
+
+	// Custom handler for the root path
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		if r.URL.Path == "/" {
+			// Serve the index.html file
+			indexPath := filepath.Join(dir, "index.html")
+			if _, err := os.Stat(indexPath); os.IsNotExist(err) {
+				http.NotFound(w, r)
+				return
+			}
+			http.ServeFile(w, r, indexPath)
+		} else {
+			// Serve other files
+			fileServer.ServeHTTP(w, r)
+		}
+	})
+
+	// Get the absolute path of the directory
+	absDir, err := filepath.Abs(dir)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Output the directory being served
+	log.Printf("Serving files from %s on http://localhost:8080\n", absDir)
+
+	// Start the HTTP server
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
