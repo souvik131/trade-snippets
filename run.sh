e0d3ce33277985542016bcc24e8ef52019b60418
Souvik Das <souvik131@gmail.com>
2025-02-05 17:56:19 +0530

    [fix] upload do

--- run.sh
+++ run.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
-
-git pull origin do;docker compose up -d --force-recreate --build fetch;docker system prune -af;
+cd /root/app;
+git pull origin do;docker compose up -d --force-recreate --build fetch;
+docker system prune -af;
