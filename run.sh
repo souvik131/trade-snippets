d8a9039cf41c8595576ced40c60a0ee66b5c607b
Souvik Das <souvik131@gmail.com>
2025-02-10 10:58:58 +0530

    [refactor] grafana

--- run.sh
+++ run.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
 cd /root/app;
-git pull origin do;docker compose up -d --force-recreate --build fetch;
+git add .;
+git commit -m "[refactor] grafana";
+git pull origin do;
+git push origin do;
+docker compose up -d --force-recreate --build fetch;
 docker system prune -af;
