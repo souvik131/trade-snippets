3b0f3eaee6318721a617731762d21135a25dd710
Souvik Das <souvik131@gmail.com>
2025-02-11 17:39:02 +0530

    [refactor] sh

--- run.sh
+++ run.sh
@@ -1,8 +1,33 @@
 #!/bin/sh
-cd /root/app;
+cd /root/app
+sudo docker compose stop
+
+
+
+sudo docker system prune -af
+sudo docker volume prune -f
+sudo docker image prune -f
+sudo docker container prune -f
+sudo docker network prune -f
+
+rm -rf /var/snap/docker/common/var-lib-docker/overlay2/*
+sudo snap stop docker 
+sudo snap remove docker --purge
+
+
+
+
 git add .;
 git commit -m "[refactor] grafana";
 git pull origin do;
 git push origin do;
-docker compose up -d --force-recreate --build fetch;
-docker system prune -af;
+
+
+sudo snap install docker
+snap restart docker
+sudo compose up -d --force-recreate --build
+sudo docker system prune -af
+sudo docker volume prune -f
+sudo docker image prune -f
+sudo docker container prune -f
+sudo docker network prune -f
